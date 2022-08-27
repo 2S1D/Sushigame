b1fd821f0e985529cdd7b9a684b3bf0be7a41f83
hyeyeon059 <leehuijuan0912@gmail.com>
2022-08-27 16:43:21 +0900

    드래그 블록 스냅기능 구현

--- Assets/02. Scripts/BlockMove.cs
+++ Assets/02. Scripts/BlockMove.cs
@@ -5,9 +5,30 @@ using UnityEngine.EventSystems;
 
 public class BlockMove : MonoBehaviour
 {
+    [SerializeField] private int snapOffset = 30;
+
+    //public Vector2Int blockCount { private set; get; }
+    public BlockPuzzle puzzle;
+
     void OnMouseDrag()
     {
         transform.position = GetMousePos();
+    }
+
+    private void OnMouseUp()
+    {
+        /*float x = Mathf.RoundToInt(transform.position.x -blockCount.x % 2 * 0.5f) + blockCount.x % 2 * 0.5f;
+        float y = Mathf.RoundToInt(transform.position.x -blockCount.x % 2 * 0.5f) + blockCount.x % 2 * 0.5f;
+
+        transform.position = new Vector3(x, y, 0);*/
+    }
+
+    private void OnMouseDown()
+    {
+        if (!CheckSnapBlock())
+        {
+            transform.SetParent(puzzle.blockPosition.transform);
+        }
     }
 
     Vector3 GetMousePos()
@@ -15,5 +36,23 @@ public class BlockMove : MonoBehaviour
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0;
         return mousePos;
+    }
+
+    bool CheckSnapBlock()
+    {
+        for (int i = 0; i < puzzle.blockPosition.transform.childCount; i++)
+        {
+            if (puzzle.blockPosition.transform.GetChild(i).childCount != 0)
+            {
+                continue;
+            }
+            else if (Vector2.Distance(puzzle.blockPosition.transform.GetChild(i).position, transform.position) < snapOffset)
+            {
+                transform.SetParent(puzzle.blockPosition.transform.GetChild(i).transform);
+                transform.localPosition = Vector3.zero;
+                return true;
+            }
+        }
+        return false;
     }
 }
