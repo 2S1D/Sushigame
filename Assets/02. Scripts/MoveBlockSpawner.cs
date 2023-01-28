68fd199b69999920c3ed3ba70504de8b68d8589e
hyeyeon059 <leehuijuan0912@gmail.com>
2023-01-28 22:55:25 +0900

    스테이지 2 완성

--- Assets/02. Scripts/MoveBlockSpawner.cs
+++ Assets/02. Scripts/MoveBlockSpawner.cs
@@ -6,19 +6,15 @@ using UnityEngine.UI;
 public class MoveBlockSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] sushiPre;
+    private int value;
 
     private void Start()
     {
-        Debug.Log(11);
-        sushiPre[0].SetActive(true);
-        sushiPre[1].SetActive(true);
-        /*sushiPre[2].SetActive(true);
-        sushiPre[3].SetActive(true);
-        sushiPre[4].SetActive(true);
-        sushiPre[5].SetActive(true);
-        sushiPre[6].SetActive(true);
-        sushiPre[7].SetActive(true);
-        sushiPre[8].SetActive(true);
-        sushiPre[9].SetActive(true);*/
+        value = sushiPre.Length;
+
+        for(int i = 0; i < value; i++)
+        {
+            sushiPre[i].SetActive(true);
+        }
     }
 }
